e68361fb8dfe8066fd7260717a858c26bf8fcf1b
Miao Cai <miaocai@slu.edu>
2019-10-14 19:09:27 -0500

    update segment function to account for initial 0s and large jump between 2 non-zero speeds

--- R/segment.R
+++ R/segment.R
@@ -12,9 +12,8 @@
 #' @examples segment(dat$speed, 30, d$time_diff)
 
 segment = function(speed, threshold, time_diff) {
-  ## Replace very long single points
-  speed[time_diff >= threshold] <- 0
-  ## First, replacing stretches of less than "threshold" consecutive 0 speeds by 1s
+  speed1 = speed 
+  speed[time_diff >= threshold] <- 0 # Replace very long single points
   r1 = rle(speed != 0)
   r1$values <- replicate(length(r1$values), 1)
   r1$values <- cumsum(r1$values)
@@ -22,10 +21,13 @@ segment = function(speed, threshold, time_diff) {
   dat_tmp1 <- data.table::data.table(speed, order_tmp, time_diff)
   dat_tmp2 <- dat_tmp1[,.(sumdiff = sum(time_diff)), by = order_tmp]
   r2 = rle(speed != 0)
+  first_rle = r2$values[1]
   r2$values[r2$values == 0 & dat_tmp2$sumdiff < threshold] <- TRUE
-  r2 <- inverse.rle(r2)
-  r2 <- rle(r2)
-  ## Then numbering consecutive stretches of non-zero values
+  r2$values[1] = first_rle
+  r2 <- rle(inverse.rle(r2))
   r2$values[r2$values] = cumsum(r2$values[r2$values])
-  return(inverse.rle(r2))
+  id = inverse.rle(r2)
+  jump_speed = which(id == 0 & speed1 != 0)
+  id[jump_speed] = id[jump_speed + 1]
+  return(id)
 }
